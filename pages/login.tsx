4a52586f121d923e63ffd5650e1a438d5eaef9a6
Nhung Le <nhung.le@asnet.com.vn>
2022-05-24 22:16:55 +0700

    Fix build failed

--- pages/login.tsx
+++ pages/login.tsx
@@ -8,13 +8,12 @@ import { setLocalStorage } from 'services/localstorage';
 import Spinner from 'components/Spinner';
 
 import { NotificationManager } from 'react-notifications';
-import { authContext } from 'context/authContext';
 import { useRouter } from 'next/router';
 import withPrivateRoute from 'components/withPrivateRoute';
-import Head from 'next/head'
+import Head from 'next/head';
 
-const Login = ({ setToken }: {setToken: Function}) => {
-  const router = useRouter()
+const Login = ({ setToken }: { setToken: Function }) => {
+  const router = useRouter();
   const [values, setValues] = useState<User>({
     email: '',
     password: '',
@@ -25,10 +24,10 @@ const Login = ({ setToken }: {setToken: Function}) => {
       NotificationManager.error(err?.response?.data?.message, null, 3000);
     },
     onSuccess: (res) => {
-      setToken?.(res?.data?.data?.token as string)
-      setLocalStorage('accessToken', res?.data?.data?.token)
-      router.push('/admin/dashboard')
-    }
+      setToken?.(res?.data?.data?.token as string);
+      setLocalStorage('accessToken', res?.data?.data?.token);
+      router.push('/admin/dashboard');
+    },
   });
 
   const [errors, setErrors] = useState<User>({
@@ -66,40 +65,47 @@ const Login = ({ setToken }: {setToken: Function}) => {
   };
 
   return (
-    <Head>
+    <>
+      <Head>
         <title>Login</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
       </Head>
-    <section className='md:max-w-[50%] mx-auto mt-[200px]'>
-      <div className='mb-[30px]'>
-        <Textbox
-          placeholder='Email'
-          onChange={handleOnChange}
-          name='email'
-          value={values?.email}
-        />
-        <p className='text-[red]'>{errors?.email}</p>
-      </div>
+      <section className='md:max-w-[50%] mx-auto mt-[200px]'>
+        <div className='mb-[30px]'>
+          <Textbox
+            placeholder='Email'
+            onChange={handleOnChange}
+            name='email'
+            value={values?.email}
+          />
+          <p className='text-[red]'>{errors?.email}</p>
+        </div>
 
-      <div className='mb-[30px]'>
-        <Textbox placeholder='Password' type='password' name='password' onChange={handleOnChange}/>
-        <p className='text-[red]'>{errors?.password}</p>
-      </div>
+        <div className='mb-[30px]'>
+          <Textbox
+            placeholder='Password'
+            type='password'
+            name='password'
+            onChange={handleOnChange}
+          />
+          <p className='text-[red]'>{errors?.password}</p>
+        </div>
 
-      <button
-        className='bg-orange-400 uppercase text-white w-full h-[40px] rounded-[5px]'
-        onClick={handleLogin}
-      >
-        {isLoading ? <Spinner/> : "Login"}
-      </button>
-      <p className='mt-[10px] text-right !text-gray-800'>
-        Don&apos;t have a account{' '}
-        <Link href='/signup'>
-          <span className='!text-orange-400'>Signup</span>
-        </Link>{' '}
-      </p>
-    </section>
+        <button
+          className='bg-orange-400 uppercase text-white w-full h-[40px] rounded-[5px]'
+          onClick={handleLogin}
+        >
+          {isLoading ? <Spinner /> : 'Login'}
+        </button>
+        <p className='mt-[10px] text-right !text-gray-800'>
+          Don&apos;t have a account{' '}
+          <Link href='/signup'>
+            <span className='!text-orange-400'>Signup</span>
+          </Link>{' '}
+        </p>
+      </section>
+    </>
   );
-}
+};
 
 export default withPrivateRoute(Login);

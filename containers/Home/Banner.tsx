29a760dd271353dece2a0c734a17beb877d37c3f
Nhung Le <nhung.le@asnet.com.vn>
2022-06-01 19:38:49 +0700

    Add another banner image

--- containers/Home/Banner.tsx
+++ containers/Home/Banner.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 const Banner = () => {
   return <div className="relative h-[500px] w-[100vw]">
-    <Image layout='fill' alt='home-banner' src="https://skylandvietnam.vn/wp-content/uploads/2018/02/simcity-premier-homes2.jpg"/>
+    <Image layout='fill' alt='home-banner' src="https://skylandvietnam.vn/wp-content/uploads/2019/04/04022018172810.jpg"/>
   </div>
 }
 

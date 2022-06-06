c0e94ad734f3c1ff44fc47b77acfef371c973d59
Nhung Le <nhung.le@asnet.com.vn>
2022-06-06 20:29:36 +0700

    Split components

--- containers/Home/ProjectsSection.tsx
+++ containers/Home/ProjectsSection.tsx
@@ -2,47 +2,47 @@ import { memo, useMemo } from 'react';
 import Container from '../../components/Container';
 import ProjectItem from './ProjectItem';
 
+const projects = [
+  {
+    id: '1',
+    city: 'Ho chi minh',
+    title: 'Lorem ipsum dolor sub amet',
+    image:
+      'https://toprealty.vn/mp-up/2020/03/quy-trinh-cac-buoc-trien-khai-du-an-dau-tu-xay-dung-cong-trinh.jpg',
+  },
+  {
+    id: '2',
+    city: 'Ho chi minh',
+    title: 'Lorem ipsum dolor sub amet',
+    image:
+      'https://toprealty.vn/mp-up/2020/03/quy-trinh-cac-buoc-trien-khai-du-an-dau-tu-xay-dung-cong-trinh.jpg',
+  },
+  {
+    id: '3',
+    city: 'Ho chi minh',
+    title: 'Lorem ipsum dolor sub amet',
+    image:
+      'https://toprealty.vn/mp-up/2020/03/quy-trinh-cac-buoc-trien-khai-du-an-dau-tu-xay-dung-cong-trinh.jpg',
+  },
+];
+
+const ProjectList = memo(() => {
+  return (
+    <div className='flex justify-between text-center flex-wrap'>
+      {projects.map((item, index) => (
+        <ProjectItem item={item} key={index} />
+      ))}
+    </div>
+  )
+})
+
 const ProjectSection = () => {
-  const projects = [
-    {
-      id: '1',
-      city: 'Ho chi minh',
-      title: 'Lorem ipsum dolor sub amet',
-      image:
-        'https://toprealty.vn/mp-up/2020/03/quy-trinh-cac-buoc-trien-khai-du-an-dau-tu-xay-dung-cong-trinh.jpg',
-    },
-    {
-      id: '2',
-      city: 'Ho chi minh',
-      title: 'Lorem ipsum dolor sub amet',
-      image:
-        'https://toprealty.vn/mp-up/2020/03/quy-trinh-cac-buoc-trien-khai-du-an-dau-tu-xay-dung-cong-trinh.jpg',
-    },
-    {
-      id: '3',
-      city: 'Ho chi minh',
-      title: 'Lorem ipsum dolor sub amet',
-      image:
-        'https://toprealty.vn/mp-up/2020/03/quy-trinh-cac-buoc-trien-khai-du-an-dau-tu-xay-dung-cong-trinh.jpg',
-    },
-  ];
-
-  const renderList = useMemo(
-    () => (
-      <div className='flex justify-between text-center flex-wrap'>
-        {projects.map((item, index) => (
-          <ProjectItem item={item} key={index} />
-        ))}
-      </div>
-    ),
-    [projects]
-  );
-
+  
   return (
     <section className='bg-gray-200 pb-[20px]'>
       <Container>
         <h2 className='uppercase text-[32px]'>Projects</h2>
-        {renderList}
+        <ProjectList />
       </Container>
     </section>
   );

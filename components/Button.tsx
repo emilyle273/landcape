0976c9a50fc75073dc3b1b4a63e6c76737726c3e
Nhung Le <nhung.le@asnet.com.vn>
2022-06-06 20:20:29 +0700

    Fix build

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,6 +1,6 @@
 import { memo } from 'react'
 
-const Button = ({ ...rest, children }) => {
+const Button = ({ children, ...rest }) => {
   <button
     className='bg-orange-700 uppercase text-white w-[105px] lg:h-[30px] h-[48px] rounded-[5px]'
     {...rest}

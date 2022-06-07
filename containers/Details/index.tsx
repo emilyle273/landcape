1c51ee850a35ac8169ecec498cf9bba6168b4ac6
Nhung Le <nhung.le@asnet.com.vn>
2022-06-07 20:28:38 +0700

    Improve page speed

--- containers/Details/index.tsx
+++ containers/Details/index.tsx
@@ -3,6 +3,7 @@ import Container from 'components/Container';
 import Banner from './Banner';
 import { News } from 'types';
 import Map from 'components/Map';
+import Image from 'next/image';
 
 const Details = ({ land }: { land: News }) => {
   return (
@@ -13,9 +14,14 @@ const Details = ({ land }: { land: News }) => {
           <h2 className='text-xl text-center text-white uppercase mb-[20px]'>
             {land?.title}
           </h2>
+          <div className="w-full h-[auto]">
+            <Image layout="fill" objectFit="contain" src={land?.image?.[0]} alt='detail-1'/>
+          </div>
           <img className='w-full' src={land?.image?.[0]} alt='detail-1' />
           <p className='my-[30px]'>{land?.description}</p>
-          <img className='w-full' src={land?.image?.[1]} alt='detail-1' />
+          <div className="w-full h-[auto]">
+            <Image layout="fill" objectFit="contain" src={land?.image?.[1]} alt='detail-1'/>
+          </div>
           {land?.location && (
             <Map location={land?.location} draggable={false} />
           )}
--- containers/Home/Banner.tsx
+++ containers/Home/Banner.tsx
@@ -3,9 +3,7 @@ import Image from 'next/image'
 import banner from 'public/banner.webp'
 
 const Banner = () => {
-  return <div className="relative h-[500px] w-[100vw]">
-    <Image objectFit="contain" layout='fill' alt='home-banner' src={banner} />
-  </div>
+  return <div className="relative h-[500px] w-[100vw] banner-home"/>
 }
 
 export default memo(Banner)
